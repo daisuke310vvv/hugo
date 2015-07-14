b548d43eb78897f9dad1221a180cceb6e12a8bbb
Daisuke Sato <daisuke.sato1991@gmail.com>
2015-07-14 21:55:52 +0900

    created first post

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
+#deploy.shの中身
 #!/bin/bash
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project. 
-hugo # if using a theme,  replace by `hugo -t <yourtheme>`
+hugo -t beg # if using a theme,  replace by `hugo -t <yourtheme>`
+
+#↑ここを変更しましょう！！
 
 # Go To Public folder
 cd public
@@ -17,7 +20,7 @@ then msg="$1"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
+#Push source and build repos.
 git push origin master
 
 # Come Back
